d6c082b38018fae0e43fddae2aec8f50c9058f53
Ari Charikar <ari.charikar1@gmail.com>
2022-05-18 16:38:44 -0400

    Completed quote generator project

---
+++ quote-generator/quotes.js
@@ -0,0 +1,18 @@
+const localQuotes = [
+    {
+        text: 'Genius is blah blah blah',
+        author: 'Thomas Edison'
+    },
+    {
+        text: 'Patience is a virtue probably',
+        author: 'The Buddha, maybe'
+    },
+    {
+        text: 'A house divided is not good or something',
+        author: 'Abraham Lincoln'
+    },
+    {
+        text: 'It sucks being named after a bear and this is a longer quote just for testing purposes so that I can be sure it is more than 50 characters',
+        author: 'Yogi Berra '
+    }
+]
